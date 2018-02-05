f3e6091df61022f6120084f718a2f1094f42cf2d
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-02-05 16:42:01 +0530

    sure1.c

---
+++ sure1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main()
+ {
+ int a,b,c=0;
+ printf("enter the natural number");
+ scanf("%d",&a);
+ printf("enter integer%d",a);
+ for(b=0;b<=a;b++)
+{
+printf("%d",b);
+c=c+b;
+}
+printf("sum of numbers=%d",c);
+scanf("sum of numbers%d",c);
+return 0;
+}
+ 
+ 
